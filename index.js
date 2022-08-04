b87aac4d744e6b5c8f1108d26be5c9fe20d2904c
1sebastiaojunior <ssqjr.ti@gmail.com>
2022-08-04 10:45:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+const Pergunta = require("./database/Pergunta")
+
+connection
+.authenticate()
+.then(() => {
+    console.log("conexão realizada com sucesso!")
+})
+.catch((msgErro) => {
+    console.log(msgErro);
+})
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Rotas
+app.get("/",(req, res) => {
+    Pergunta.findAll({ raw: true}).then(perguntas => {
+        res.render("index",{
+            perguntas: perguntas
+        });
+    });
+});
+
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar");
+});
+
+app.post("/salvarpergunta",(req, res) => {
+    var title = req.body.title;
+    var description = req.body.description;
+
+    Pergunta.create({
+        title: title,
+        description: description
+    }).then(() => {
+        res.redirect("/");
+    });
+});
+
+app.listen(8080,() => {
+    console.log("App rodando!!!");
+});
