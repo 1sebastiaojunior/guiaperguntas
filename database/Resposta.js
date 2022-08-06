01ce7f9cb8482f20a4e41e106b171843e7c5a03b
1sebastiaojunior <ssqjr.ti@gmail.com>
2022-08-06 10:08:34 -0400

    projeto finalizado

--- database/Resposta.js
+++ database/Resposta.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const connection = require("./database");
 
 const Resposta = connection.define("respostas", {
-    body: {
+    corpo: {
         type: Sequelize.TEXT,
         allowNull: false
     },
