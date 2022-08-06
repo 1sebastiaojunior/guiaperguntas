698e201452a0767aeba4f394b6ac1f1f91de68a3
1sebastiaojunior <ssqjr.ti@gmail.com>
2022-08-05 20:18:44 -0400

    model reposta

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
-const Pergunta = require("./database/Pergunta")
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
 
 connection
 .authenticate()
@@ -22,7 +23,9 @@ app.use(bodyParser.json());
 
 //Rotas
 app.get("/",(req, res) => {
-    Pergunta.findAll({ raw: true}).then(perguntas => {
+    Pergunta.findAll({ raw: true, order:[
+        ['id', 'DESC']
+    ]}).then(perguntas => {
         res.render("index",{
             perguntas: perguntas
         });
@@ -43,6 +46,21 @@ app.post("/salvarpergunta",(req, res) => {
     }).then(() => {
         res.redirect("/");
     });
+});
+
+app.get("/pergunta/:id",(req, res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined){
+            res.render("pergunta", {
+                pergunta: pergunta
+            });
+        }else{
+            res.redirect("/");
+        }
+    }); 
 });
 
 app.listen(8080,() => {
